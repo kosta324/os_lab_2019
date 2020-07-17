2526b2e6edc63d9f96b13382c00e0fe2921a47c9
kosta324 <kosta324@yandex.ru>
2020-07-17 08:54:33 +0000

    изменен revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
 #include "revert_string.h"
+#include "string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str);
+    int i = 0;
+    char temp;
+    for(i = 0; i < len/2; i++){
+        temp = *(str + i);
+        *(str + i) = *(str + len - 1 - i);
+        *(str + len - 1 - i) = temp;
+    }
 }
-
